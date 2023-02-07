35bfbac1f747d1593a2d5610c6fd858d23a4f9f9
fouzia <fouziasultana470@gmail.com>
2023-02-07 16:30:36 +0530

    let and const added

---
+++ 3-variables/let and const.js
@@ -0,0 +1,29 @@
+//before es6 version,there is only one way to create
+//variable in javascript is using var keyword
+var myvariable="some data"   
+
+//in es6 version let and const are introduced
+//variables
+
+//var declaration with value
+var myname = "md"
+ 
+//var redeclaration with some variable identifier
+var myname = "md fouzia"
+
+//let declation with values 
+let myhometown = "guntur"
+
+//we cant redeclare let variables again
+//let myhometown = "ongole" //javascript will throw errors 
+
+// but we reassign new value to let or var 
+myname="md fouzia sultana"
+myhometown="hyderabad"
+
+
+//for var and let we can reassign values(remove old value and update new value)
+
+const dob = 29-6-1999
+//for const we need to declare and assign the value 
+// we cant reassign the const value
