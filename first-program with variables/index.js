5f344c3f6691bb2728257d360408b90106ae5150
fouzia <fouziasultana470@gmail.com>
2023-02-07 19:52:07 +0530

    program added

---
+++ first-program with variables/index.js
@@ -0,0 +1,21 @@
+var initial = "Md";
+let firstname = "Fouzia";
+let lastname = "Sultana";
+
+const city = "Guntur";
+
+const friends = [
+    'Uzma',
+    'Arshiya',
+    'Sheema'
+
+];
+
+const myDetails = {
+    fullname:  `${firstname} ${lastname}, ${initial}`,
+    designation:'programmer',
+    friends:friends,
+    city:city
+}
+
+console.log(Details);
